856d27d60c2546df7458d7b6238dcc01581cfc5f
hill.xu <13856583506@163.com>
2022-01-25 05:22:43 +0800

    terminal基础文字新增、编辑、删除处理

--- src/component/xtermIns/index.tsx
+++ src/component/xtermIns/index.tsx
@@ -11,7 +11,7 @@ function XtemIns() {
     const xtermIns: Terminal = new Terminal({
       rendererType: 'canvas', // 渲染类型
       convertEol: true, // 启用是，光标设置为下一行的开头
-      disableStdin: false, // 禁用输入
+      disableStdin: true, // 禁用输入
       cursorStyle: 'block', // 光标样式
       cursorBlink: true, // 光标闪烁
       theme: {
@@ -32,16 +32,95 @@ function XtemIns() {
       }
     })
 
+
     // 获取xterm键盘输入
     // 换行问题
     // 中间插入问题
+    let cmd: string[] = []; // 操作数组
+    let curIndex: number = 0; // 光标所在位置
+    let lastInputValue = ''; // 最后一个输入的值
+    // xtermIns.onData((value) => {
+    //   console.log(value);
+    // })
     xtermIns.onKey(({key, domEvent}) => {
-      console.log(key,domEvent);
-      xtermIns.write(key)
+      let visibleTextArr = cmd.filter(item => !(item === '\x1B[C') && !(item === '\x1B[D') && !(item === '\x7F'))
+      const { keyCode } = domEvent
+      const disabledKey = [40, 38]
+      // 正常情况下禁用上移下移
+      if (disabledKey.includes(keyCode)) {
+        return
+      }
+      // 左移/右移边界处理
+      if (key === '\x1B[C' && curIndex === visibleTextArr.length) {
+        return
+      }
+      if (key === '\x1B[D' && curIndex === 0) {
+        return
+      }
+      if (key === '\x1B[C') { // 右移
+        curIndex ++;
+      } else if (key === '\x1B[D'){ // 左移
+        curIndex --;
+      }
+      if (key !== '\x7F') { // 删除的可以
+        cmd.push(key)
+      }
+      switch(keyCode) {
+        case 8:
+          // 删除处理
+          // 禁止删除前缀
+          if (cmd.length === 0 || curIndex === 0) {
+            return
+          }
+          // 中间删除
+          if (curIndex < visibleTextArr.length) {
+            const afterVisibleTextArr = visibleTextArr.slice(curIndex)
+            visibleTextArr.splice(curIndex - 1, 1);
+            cmd = [...visibleTextArr];
+            ([...afterVisibleTextArr]).forEach(() => {
+              xtermIns.write('\x1B[C') // 右移光标
+            });
+            ([...afterVisibleTextArr, '']).forEach(() => {
+              xtermIns.write('\b \b') // 删除文字
+            })
+            xtermIns.write(afterVisibleTextArr.join('')); // 添加文字
+            ([...afterVisibleTextArr]).forEach(() => {
+              xtermIns.write('\x1B[D') // 左移动光标
+            });
+            curIndex --;
+            return 
+          }
+          // 删除最新的值
+          curIndex --;
+          const index = cmd.lastIndexOf(lastInputValue)
+          cmd.splice(index, 1)
+          lastInputValue = cmd[index - 1]
+          xtermIns.write('\b \b')
+          break;
+        default:
+          if (!(key === '\x1B[C') && !(key === '\x1B[D')) {
+            // 中间插入
+            if (curIndex < visibleTextArr.length) {
+              visibleTextArr.splice(curIndex, 0, key)
+              const afterVisibleTextArr = visibleTextArr.slice(curIndex + 1)
+              cmd = [...visibleTextArr]
+              xtermIns.write(key + afterVisibleTextArr.join(''));
+              afterVisibleTextArr.forEach(() => {
+                xtermIns.write('\x1B[D') // 修改光标位置
+              })
+              curIndex ++;
+              return 
+            }
+            curIndex ++;
+            lastInputValue = key
+          }
+          xtermIns.write(key);
+      }
     })
 
     setXtermIns(() => {
-      xtermIns.write(`$ `)
+      xtermIns.writeln('\x1b[1;1;32mwellcom to web terminal!\x1b[0m')
+      xtermIns.write(`\r\n$ `)
       return xtermIns
     })
     
